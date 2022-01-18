daa7c933cecae5b74cd6256ea0381a18f27ffb94
Suryaa0000 <96015297+Suryaa0000@users.noreply.github.com>
2022-01-18 22:36:14 +0530

    Add files via upload

---
+++ Project_1_MyCaptain.cpp
@@ -0,0 +1,29 @@
+// "C++ program to find size in bytes" //
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//     cout<<"Size of char :"<<sizeof(char)<<" byte"<<endl;
+//     cout<<"Size of int :"<<sizeof(int)<<" byte"<<endl;
+//     cout<<"Size of float :"<<sizeof(float)<<" byte"<<endl;
+//     cout<<"Size of double :"<<sizeof(double)<<" byte"<<endl;
+//     return 0;
+// }
+
+
+// MULTIPLICATION OF TWO NUMBERS //
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter your first number :"<<a;
+    cin>>a;
+    cout<<"Enter your second number :"<<b;
+    cin>>b;
+    cout<<"Product of your number is :"<<a*b;
+
+    return 0;
+}
